f06661597a9b3ca1277c7a10a223b20f29758603
Prashant mishra <110593606+Pm09568@users.noreply.github.com>
2023-04-29 00:15:10 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,14 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-       Scanner sc=new Scanner(System.in);
-        System.out.println("enter a");
-       int a=sc.nextInt();
-        System.out.println("enter b");
-       int b=sc.nextInt();
-        System.out.println("a= "+a + " b= "+b);
-//        System.out.println("b = "+b);
-        System.out.println("HELLO JAVA PROGARAMING");
+        System.out.println("Hello world!");
     }
 }
