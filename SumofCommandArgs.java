8867e26f5dc70fdc04a140765d11b65569afd602
Prashant mishra <110593606+Pm09568@users.noreply.github.com>
2023-05-13 22:16:19 +0530

    Add files via upload

---
+++ SumofCommandArgs.java
@@ -0,0 +1,10 @@
+
+//public class SumofCommandArgs {
+//    public static void main(String args[]){
+//        int sum=0;
+//        int num1=integer.parseInt(args[0]);
+//        int num2=integer.parseInt(args[1]);
+//        Sum =num1+num2;
+//        System.out.println("sum "+sum);
+//    }
+//}
