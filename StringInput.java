ea372255045e040879035c7a82ee85a1ca8c256d
Prashant mishra <110593606+Pm09568@users.noreply.github.com>
2023-05-15 22:15:47 +0530

    Add files via upload

---
+++ StringInput.java
@@ -0,0 +1,8 @@
+ import java.util.*;
+public class StringInput {
+    public static void main(String agrs[]){
+      Scanner sc= new Scanner(System.in);
+      String name=sc.nextLine();
+      System.out.println(name);
+    }
+}
