0497f78fea8e7adcdd450330b57dbeffb5d952a5
Prashant mishra <110593606+Pm09568@users.noreply.github.com>
2023-06-16 22:34:14 +0530

    Add files via upload

---
+++ ProtectAccess.java
@@ -0,0 +1,9 @@
+package pac;
+//  this program is  protected access modifier related to pac2 B class
+public class ProtectAccess {
+       protected   void display()
+        {
+            System.out.println("Heloo Prashant Mishra");
+        }
+    }
+
